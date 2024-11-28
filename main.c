da0ce26665cbbe3ee23d7d5e9c2049b47d66c4ad
Joao Bettu <joaobettuvictor@gmail.com>
2024-11-27 21:03:06 -0300

    Removido vetor e ajustado total de carac

--- main.c
+++ main.c
@@ -24,13 +24,11 @@ typedef struct mudas{
 } Sentinela;
 
 // Varáveis globais:
-int total_caracteres;
-Codificacao *vetor_caracteres;
 Sentinela pontas;
 
 // Assinatura das funções:
 void le_palavra();
-void monta_lista(FILE *p);
+void monta_lista(FILE *p, int total_caracteres);
 int tem_letra(char c);
 void aumenta_freq(char c);
 void ordena_freq();
@@ -54,6 +52,7 @@ int main(){
 // Funções:
 void le_palavra(){
     FILE *fp;
+    int total_caracteres;
 
     fp = fopen("/home/joaobettu/Documentos/UFFS/2024-2/POD/TF/amostra.txt", "r");
     if(fp==NULL){
@@ -62,28 +61,20 @@ void le_palavra(){
     }
 
     fscanf(fp, "%d\n", &total_caracteres);
-    monta_lista(fp);
+    monta_lista(fp, total_caracteres);
 
     fclose(fp);
 }
-void monta_lista(FILE *p){
+void monta_lista(FILE *p, int total_caracteres){
     char c;
 	Lista *novo_no;
 
     pontas.head = NULL;
     pontas.tail = NULL;
 
-    //vetor_caracteres = (Codificacao *)malloc(sizeof(Codificacao) * total_caracteres);
 
     for(int i = 0; i < total_caracteres; i++){
         c = fgetc(p);
-        /*if(tem_letra(c)){
-            aumenta_freq(c);
-        }else{
-            vetor_caracteres[i].caractere = c;
-            vetor_caracteres[i].freq = 1;
-            null_folhas(&vetor_caracteres[i]);
-        }*/
         if(tem_letra(c)){
 			aumenta_freq(c);
         }else{
@@ -106,11 +97,6 @@ void monta_lista(FILE *p){
     print_lista();
 }
 int tem_letra(char c){
-    /*for(int i = 0; i < total_caracteres; i++){
-        if(c == vetor_caracteres[i].caractere){
-            return 1;
-        }
-    }*/
     Lista *aux;
 
 	for(aux = pontas.head; aux!=NULL; aux = aux->next){
@@ -122,14 +108,7 @@ int tem_letra(char c){
     return 0;
 }
 void aumenta_freq(char c){
-
-    /*for(int i = 0; i < total_caracteres; i++){
-        if(vetor_caracteres[i].caractere == c){
-            vetor_caracteres[i].freq++;
-        }
-    }*/
-
-   	Lista *aux;
+    Lista *aux;
 
 	if(pontas.head==NULL){
 		printf("Lista vazia!\n");
@@ -144,27 +123,6 @@ void aumenta_freq(char c){
 }
 void ordena_freq(){
     // Bubble sort
-    /*for(int i = 0; i < total_caracteres; i++){
-        int houve_troca = 0;
-        for(int j = i + 1; j < total_caracteres; j++){
-            if(vetor_caracteres[i].freq == 0){
-                troca_posi(&vetor_caracteres[j], &vetor_caracteres[i]);
-                houve_troca++;
-                continue;
-            }
-            if(vetor_caracteres[i].freq > vetor_caracteres[j].freq){
-                if(vetor_caracteres[j].freq == 0){
-                    continue;
-                }
-                troca_posi(&vetor_caracteres[i], &vetor_caracteres[j]);
-                houve_troca++;
-            }
-        }
-        if(houve_troca == 0){
-            break;       
-        }
-    }*/
-
     Lista *aux, *robin;
 
 	for(aux = pontas.tail; aux!=NULL; aux = aux->prev){
@@ -190,36 +148,13 @@ void troca_posi(Codificacao *maior, Codificacao *menor){
     *menor = troca;
 }
 void monta_arvore(){
-    Codificacao ramo;
-
-    for(int i = 0; i < total_caracteres; i++){
-        if(vetor_caracteres[i].freq == 0){
-            break;
-        }
-        ramo.freq = vetor_caracteres[i].freq + vetor_caracteres[i+1].freq;
-        printf("\nRamo freq: %d\n", ramo.freq);
-        ramo.caractere = ramo.freq;
-        printf("Ramo caracter: %c\n", ramo.caractere);
-        ramo.esquerda = &vetor_caracteres[i];
-        ramo.direita = &vetor_caracteres[i+1];
-        for(int j = 0; j < total_caracteres; j++){
-            if(vetor_caracteres[j].freq == 0){
-                vetor_caracteres[j] = ramo;
-            }
-        }
-    }
-    printf("\nRamos:\n");
-    //print_vetor();
+    
 }
 void null_folhas(Codificacao *galho){
     galho->direita = NULL;
     galho->esquerda = NULL;
 }
 void print_lista(){
-    /*for(int i = 0; i < total_caracteres; i++){
-        printf("Letra: %c\nFrequência: %d\n", vetor_caracteres[i].caractere, vetor_caracteres[i].freq);
-    }*/
-
     Lista *aux;
 
 	for(aux = pontas.head; aux!=NULL; aux = aux->next){
